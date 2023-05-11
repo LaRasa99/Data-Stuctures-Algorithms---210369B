81a52d0eed0b3f764266659419d0538b03cae3ce
LaRasa99 <96112509+LaRasa99@users.noreply.github.com>
2023-05-11 14:35:43 +0530

    Update hashtable_basics.cpp

--- lab materials/hashtable_basics.cpp
+++ lab materials/hashtable_basics.cpp
@@ -1,3 +1,6 @@
+//
+// Created by DELL on 5/11/2023.
+//
 #include <cstring>
 #include <string>
 #include <iostream>
@@ -26,13 +29,15 @@ struct HashTable{
     int hashfunc(string user_name){
         int sum = 0;
         int hash = 0;
-	//add your code below
-	
-	
-	
+        //add your code below
+        for(int i = 0;i< user_name.length(); i++){
+            sum += int(user_name[i]);
+
+        }
         hash = sum % MAX_LENGTH;
 
         return hash;
+
     }
     bool is_slot_empty(int hash){
         bool empty = password[hash].empty();
@@ -43,8 +48,13 @@ struct HashTable{
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-	//add an if condition to complete the code here
-        
+        if(empty){
+            password[hash] = user_password;
+        } else {
+            cout << "Collision" << endl;
+        }
+        //add an if condition to complete the code here
+
 
     }
     void hash_lookup(string user_name){
@@ -52,10 +62,15 @@ struct HashTable{
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-	//add an if condition to complete the code here
-	
-	
-	
+        if(empty){
+            cout << "User not found";
+        }else{
+            password[hash];
+        }
+        //add an if condition to complete the code here
+
+
+
     }
     void delete_item(string user_name){
         int hash;
@@ -92,37 +107,37 @@ int main(){
         cout << "Type command: ";
         cin >> command;
         switch (command){
-        case 1:
-            /* insert the new item*/
-            cout << "Enter user name: ";
-            cin >> user_name;
-            cout << "Enter password to be saved: ";
-            cin >> password;
-            hashtbl->insert(user_name,password);
-            break;
-        case 2:
-            /* delete item */
-            cout << "Enter item to be deleted: ";
-            cin >> user_name;
-            hashtbl->delete_item(user_name);
-            break;
-        case 3:
-            /* hash lookup password*/
-            cout << "Enter user name to look up password:";
-            cin >> user_name;
-            hashtbl->hash_lookup(user_name);
-            break;
-        case 4:
-            hashtbl->print_hashtable();
-            break;
-        case -1:
-            /* hash lookup password*/
-            cout << "Exiting...\n";
-            break;
-        
+            case 1:
+                /* insert the new item*/
+                cout << "Enter user name: ";
+                cin >> user_name;
+                cout << "Enter password to be saved: ";
+                cin >> password;
+                hashtbl->insert(user_name,password);
+                break;
+            case 2:
+                /* delete item */
+                cout << "Enter item to be deleted: ";
+                cin >> user_name;
+                hashtbl->delete_item(user_name);
+                break;
+            case 3:
+                /* hash lookup password*/
+                cout << "Enter user name to look up password:";
+                cin >> user_name;
+                hashtbl->hash_lookup(user_name);
+                break;
+            case 4:
+                hashtbl->print_hashtable();
+                break;
+            case -1:
+                /* hash lookup password*/
+                cout << "Exiting...\n";
+                break;
+
         }
 
-    
+
     }
     return 0;
 }
